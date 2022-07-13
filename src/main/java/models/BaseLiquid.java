41fd6a9ba07f561c28b93b9510cd2ec6e3bfc5e2
alpineca <e.nikolov92@gmail.com>
2022-07-13 10:25:45 +0300

    BaseLiquid Model Implemented

--- src/main/java/models/BaseLiquid.java
+++ src/main/java/models/BaseLiquid.java
@@ -1,12 +1,35 @@
 package models;
 
 public class BaseLiquid {
+    private String baseName;
     private double desiredAmmount;
-    private double desiredStrength;
-    private double desiredDiluent;
-    private PropyleneGlycol desiredPG;
-    private VegetableGlycerin desiredVG;
 
+//    private double desiredStrength;
+//    private double desiredDiluent;
+//    private PropyleneGlycol desiredPG;
+//    private VegetableGlycerin desiredVG;
 
+    public String getBaseName() {
+        return baseName;
+    }
 
+    public void setBaseName(String baseName) {
+        this.baseName = baseName;
+    }
+
+    public double getDesiredAmmount() {
+        return desiredAmmount;
+    }
+
+    public void setDesiredAmmount(double desiredAmmount) {
+        this.desiredAmmount = desiredAmmount;
+    }
+
+    public BaseLiquid(String baseName, double desiredAmmount) {
+        this.baseName = baseName;
+        this.desiredAmmount = desiredAmmount;
+    }
+    public BaseLiquid(){
+
+    }
 }
